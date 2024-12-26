d479222a67b3aff00ac2c09ffa0a7595c24aa1ab
Rofiq Fadilah <121142060+mrofiqfdilah@users.noreply.github.com>
2024-12-26 18:11:49 +0700

    Update login_page.blade.php

--- resources/views/auth/login_page.blade.php
+++ resources/views/auth/login_page.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Form</title>
-    <!-- Tailwind CSS CDN -->
+    
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         // Tailwind dark mode configuration
