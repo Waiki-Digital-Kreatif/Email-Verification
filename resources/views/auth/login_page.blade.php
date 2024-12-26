56962bd5c253dc9eb6e0f908764f02dfc9cdf441
Rofiq Fadilah <121142060+mrofiqfdilah@users.noreply.github.com>
2024-12-26 18:12:00 +0700

    Update login_page.blade.php

--- resources/views/auth/login_page.blade.php
+++ resources/views/auth/login_page.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Form</title>
-    
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
-        // Tailwind dark mode configuration
+     
         tailwind.config = {
             darkMode: 'class', // Enable class-based dark mode
         };
