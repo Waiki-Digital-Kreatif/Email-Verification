82d970cecab567165cb153a1e4e2841420abd212
Rofiq Fadilah <121142060+mrofiqfdilah@users.noreply.github.com>
2024-12-31 10:41:40 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -5,22 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
     'name' => env('APP_NAME', 'Laravel'),
-
-
-
     'env' => env('APP_ENV', 'production'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
-
     'debug' => (bool) env('APP_DEBUG', false),
 
     /*
