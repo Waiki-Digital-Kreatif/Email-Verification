24afdfc56f26efd4306afe8ca640682bb9ed9894
Rofiq Fadilah <121142060+mrofiqfdilah@users.noreply.github.com>
2024-12-31 10:42:10 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -7,18 +7,6 @@ return [
     'name' => env('APP_NAME', 'Laravel'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
-    |
-    */
-
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
